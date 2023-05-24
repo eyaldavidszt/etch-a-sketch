814ae595068d7f79af00cb03fb3ff663f08032bc
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-05-25 02:46:32 +0300

    Make grid colorable

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
 //make 16x16 grid:
+function makeInit(amount) {
+    container = document.querySelector('.container');
+    for (let i = 0; i < amount; i++)
+    {
+        let col = document.createElement('div');
+        for (let j = 0; j < amount; j++)
+        {
+            let cell = document.createElement('div');
+            // every cell will have a mouseover event listener
+            cell.addEventListener('mouseover', colorDiv);
+            col.appendChild(cell);
 
+        }
+        container.appendChild(col);
+    }
+}
 
+makeInit(16);
 
 
 
@@ -9,49 +26,45 @@ generator = document.querySelector('#gen');
 generator.addEventListener('click', generateGrid)
 
 function generateGrid() {
+    let amount = parseInt(document.querySelector('.input-size').value);
+    if (amount > 100)
+    {
+        return "Can't generate >100x100 grid";
+    }
     // clear existing grid
     myDiv = document.querySelector('.container');
     myDiv.replaceChildren();
 
-    let amount = parseInt(document.querySelector('.input-size').value);
-
-    for (let i = 0; i < amount; i++)
-    {
-        // make column 
-        let col = document.createElement('div');
-        for (let j = 0; j < amount; j++)
-        {
-            let cell = document.createElement('div');
-            // every cell will have a mouseover event listener
-            cell.addEventListener('mouseover', colorDiv);
-            col.appendChild(cell);
-        }
-        container.appendChild(col);
-    }
+    makeInit(amount);
 }
 
 
 function colorDiv() {
-    if (this.style.backgroundColor == rgb(255, 255, 255))
+    console.log(this.style.backgroundColor);
+    if (this.style.backgroundColor == '')
     {
-        this.style.backgroundColor = Math.floor(Math.random() * 255);
+        let randomRed = Math.floor(Math.random() * 255);
+        let randomGreen = Math.floor(Math.random() * 255);
+        let randomBlue = Math.floor(Math.random() * 255);
+        this.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
     }
     else {
         const rgbColor = window.getComputedStyle(this)["background-color"];
-        rgbArr = rgbColor.substring(4, rgbColor.length-1).replace(/ /g, '').split(',');
-        newArr = [];
-        for (i = 0; i < rgbColor.length; i++)
+        let rgbArr = rgbColor.substring(4, rgbColor.length-1).replace(/ /g, '').split(',');
+        let newArr = [];
+        console.log(rgbArr);
+        for (i = 0; i < rgbArr.length; i++)
         {
-            if (parseInt(rgbArr[i]) - 20 > 0)
+            if (parseInt(rgbArr[i]) - 30 > 0)
             {
-                newArr.push(parseInt(rgbArr[i]) - 20);
+                newArr.push(parseInt(rgbArr[i]) - 30);
             }
             else {
                 //reduce to 0 if result would be lower than 0
-                newArr.push(parseInt(rgbArr[i]) - 20 - (parseInt(rgbArr[i])));
+                newArr.push(parseInt(rgbArr[i]) - 30 - (parseInt(rgbArr[i]) - 30));
             }
         }
-        this.style.backgroundColor = `rgb(${newArr[0]}, ${newArr[1]}, ${rgbArr[2]})`;
+        this.style.backgroundColor = `rgb(${newArr[0]}, ${newArr[1]}, ${newArr[2]})`;
     }
     return;
 }
